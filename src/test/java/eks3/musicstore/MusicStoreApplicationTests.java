a04e57730507ff76ff2a7dbd326f9cc276a40afb
Naima <124045420+naja0001@users.noreply.github.com>
2024-11-20 13:49:09 +0100

    Initial commit

---
+++ src/test/java/eks3/musicstore/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package eks3.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
